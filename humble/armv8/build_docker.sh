b00fb965abba8154524fe4ccf4df3785ca71c56f
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-05-08 19:50:22 -0700

    changed general humble for arm

--- humble/armv8/build_docker.sh
+++ humble/armv8/build_docker.sh
@@ -1,2 +1,10 @@
-docker build -f general_humble -t general_humble .
+#!/bin/bash
+distro="$1"
+if [ -z "$1" ]
+    then
+        echo "No argument given, you want make a specific docker please put the name such as limo or mg400,,, , please provide it as an argument."
+        echo "in this case , since no argument is given, I will create a general one"
+        distro="general"
+fi
 
+docker build -f ${distro}_humble -t general_humble .
